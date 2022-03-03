77dcf6c80476a167980c166c998fb4d1e7520381
celerie <wykhongryan@gmail.com>
2022-03-03 17:02:24 +0800

    Added settings button to main menu

--- lib/main.dart
+++ lib/main.dart
@@ -54,3 +54,52 @@ class MyApp extends StatelessWidget {
       );
   }
 }
+
+/* Navigation Tab Bar */
+class TabBar extends StatefulWidget {
+  const TabBar({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _TabBarState();
+}
+
+class _TabBarState extends State<TabBar> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: IndexedStack(
+          index: selectedIndex,
+          children: const <Widget>[
+            CommunityTab(),
+            MapTab(),
+            NewsTab(),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            destinations: const [
+              NavigationDestination(
+                selectedIcon: Icon(Icons.groups),
+                icon: Icon(Icons.groups_outlined),
+                label: 'Community',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.map),
+                icon: Icon(Icons.map_outlined),
+                label: 'Map',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.podcasts),
+                icon: Icon(Icons.podcasts_outlined),
+                label: 'News',
+              ),
+            ]));
+  }
+}
