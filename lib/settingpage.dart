56508f715cc7b37c2002ee2a5de66769ea2e5938
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2022-03-03 17:08:29 +0800

    Added widget for Setting Page, not yet integrated into Main

---
+++ lib/settingpage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(Setting());
+
+class Setting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text('Settings'),
+            )
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: _changehomeButton(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: _logoutButton(),
+                ),
+              ],
+            ),
+          )
+        )
+    );
+  }
+}
+
+Widget _logoutButton() {
+  return Container(
+    child: ElevatedButton(
+      onPressed: () {},
+      child: const Text('Logout'),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.blue, //Background color
+        onPrimary: Colors.black, //Text
+        fixedSize: const Size(240,50),
+        padding: const EdgeInsets.all(25),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      ),
+    ),
+  );
+}
+
+Widget _changehomeButton() {
+  return Container(
+    child: ElevatedButton(
+      onPressed: () {},
+      child: const Text('Change Home Address'),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.blue, //Background color
+        onPrimary: Colors.black, //Text
+        fixedSize: const Size(240,50),
+        padding: const EdgeInsets.all(25),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      ),
+    ),
+  );
+}
