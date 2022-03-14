0c8a4200bdf796ebd0eece3a508e84b8f443d02d
celerie <wykhongryan@gmail.com>
2022-03-15 03:08:01 +0800

    introduction page

--- lib/view/onboarding/introduction_page.dart
+++ lib/view/onboarding/introduction_page.dart
@@ -1,45 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:introscreenonboarding/intro_app.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 import 'home_page.dart';
 
 void main() => runApp(TestScreen())
 class TestScreen extends StatelessWidget {
-  final List<Introduction> list = [
-    Introduction(
-      title: 'Buy & Sell',
-      subTitle: 'Browse the menu and order directly from the application',
-      imageUrl: 'assets/images/onboarding3.png',
+
+  List<PageViewModel> listPagesViewModel = [
+    PageViewModel(
+      title: "Title of first page",
+      body: "The map tab",
+      image: Center(
+        child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg", height: 175.0),
+      ),
     ),
-    Introduction(
-      title: 'Delivery',
-      subTitle: 'Your order will be immediately collected and',
-      imageUrl: 'assets/images/onboarding4.png',
-    ),
-    Introduction(
-      title: 'Receive Money',
-      subTitle: 'Pick up delivery at your door and enjoy groceries',
-      imageUrl: 'assets/images/onboarding5.png',
-    ),
-    Introduction(
-      title: 'Finish',
-      subTitle: 'Browse the menu and order directly from the application',
-      imageUrl: 'assets/images/onboarding3.png',
-    ),
+    PageViewModel(
+      title: "Title of second page",
+      body: "The community tab",
+      image: Center(
+        child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg", height: 175.0),
+      ),
+    )
   ];
+
 
   @override
   Widget build(BuildContext context) {
-    return IntroScreenOnboarding(
-      introductionList: list,
-      onTapSkipButton: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => HomePage(),
-          ), //MaterialPageRoute
-        );
+    return IntroductionScreen(
+      pages: listPagesViewModel,
+      done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+      onDone: () {
+        // When done button is press
       },
-    );
+    ); //Material App
   }
 }
