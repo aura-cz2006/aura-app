ccc8b9caff3981c32d9446e1d892ffa911b5ee5b
nicolelst <nicolelim2608@gmail.com>
2022-03-18 22:37:57 +0800

    added detailed thread view with comment list display and comment input field

--- lib/models/thread.dart
+++ lib/models/thread.dart
@@ -1,23 +1,47 @@
+import 'package:aura/models/comment.dart';
+
 import 'user.dart';
 class Thread {
   String? id;
   String? title;
 
   String content;
-  List<String> comments = []; // todo: change this to List<Comment>
+  List<Comment> comments = [];
   User author;
   List<User> likedBy = [];
+  DateTime timestamp;
 
   // constructor
   Thread(
       this.id,
       this.title,
       this.author,
-      this.content// body, comments, author, likedBy
-      );
+      this.content,
+      this.timestamp// comments, likedBy
+      ) ;
 
   @override
-  String toString() {
-    return 'Discussion: {id: ${id ?? ""}, title: ${title ?? ""}}';
+  String toString() { //todo
+    return 'Thread: {id: ${id ?? ""}, title: ${title ?? ""}}';
+  }
+  void addComment(Comment newC) {
+    comments.add(newC);
+  }
+  bool isLikedBy(User user) {
+    if (likedBy.contains(user)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  void addLike(User user) {
+    likedBy.add(user);
+  }
+  void removeLike(User user) {
+    likedBy.remove(user);
+  }
+  int numLikes() {
+    return likedBy.length;
   }
 }
