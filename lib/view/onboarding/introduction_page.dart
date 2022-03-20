bb004c32092aa20a612607187443f4dd3f44bb4a
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2022-03-20 16:33:58 +0800

    Added "Input User's Home Address" to onboarding

--- lib/view/onboarding/introduction_page.dart
+++ lib/view/onboarding/introduction_page.dart
@@ -20,6 +20,22 @@ class IntroScreen extends StatelessWidget {
       image: Center(
         child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg", height: 175.0),
       ),
+    ),
+    PageViewModel(
+      title: "User's Home Address",
+      bodyWidget: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: homeadd_textbox(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 50),
+            child: homeadd_submitButton()
+          ),
+        ],
+      ),
+      image: Center(child: Icon(Icons.home,size: 175,color: Colors.lightBlueAccent),),
     )
   ];
 
@@ -39,4 +55,42 @@ class IntroScreen extends StatelessWidget {
     ),
     );//Material App
   }
+}
+
+Widget homeadd_textbox(){
+  return Container(
+      decoration: BoxDecoration(
+        color: Color(0x11111111),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(left: 15, right: 15, top: 5),
+        child: TextFormField(
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "Enter your home's address:",
+            )
+        ),
+      )
+  );
+}
+
+Widget homeadd_submitButton() {
+  return ElevatedButton(
+      style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0)
+              )
+          )
+      ),
+      child: Text("Submit"),
+      onPressed: (){
+        /*Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ???()),*/
+      },
+    );
 }
