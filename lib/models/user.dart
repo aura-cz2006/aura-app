fd524ebf1e6a7c649f3348a805ce60de24d22b57
celerie <wykhongryan@gmail.com>
2022-03-21 21:29:49 +0800

    merged updated user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,11 @@
 class User {
   late String id;
   late String username;
+  String homeaddress = '';
 
   User(this.id, this.username);
+
+  void updateHomeAddress(String newHomeAddress){
+    homeaddress = newHomeAddress;
+  }
 }
