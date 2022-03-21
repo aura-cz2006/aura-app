7f7ad59d5a96618279f0e75a429773bf6d91165f
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2022-03-21 22:32:59 +0800

    Changed stateless into Stateful Added textcontroller for passing HomeAddress data To-Do: 1) Configure updateHomeAddress under setState upon tapping submit button 2) Configure router upon tapping submit button

--- lib/view/onboarding/introduction_page.dart
+++ lib/view/onboarding/introduction_page.dart
@@ -4,93 +4,110 @@ import 'package:introduction_screen/introduction_screen.dart';
 import 'home_page.dart';
 
 void main() => runApp(IntroScreen());
-class IntroScreen extends StatelessWidget {
 
-  List<PageViewModel> listPagesViewModel = [
-    PageViewModel(
-      title: "Title of first page",
-      body: "The map tab",
-      image: Center(
-        child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg", height: 175.0),
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  final addresstxt_Controller = TextEditingController();
+
+  List<PageViewModel> listPagesViewModel = [];
+
+  @override
+  void initState(){
+    listPagesViewModel = [
+      PageViewModel(
+        title: "Title of first page",
+        body: "The map tab",
+        image: Center(
+          child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg", height: 175.0),
+        ),
       ),
-    ),
-    PageViewModel(
-      title: "Title of second page",
-      body: "The community tab",
-      image: Center(
-        child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg", height: 175.0),
+      PageViewModel(
+        title: "Title of second page",
+        body: "The community tab",
+        image: Center(
+          child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg", height: 175.0),
+        ),
       ),
-    ),
-    PageViewModel(
-      title: "User's Home Address",
-      bodyWidget: Column(
-        children: [
-          Padding(
+      PageViewModel(
+        title: "User's Home Address",
+        bodyWidget: Column(
+          children: [
+            Padding(
               padding: EdgeInsets.only(left: 10, right: 10),
-              child: homeadd_textbox(),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 50),
-            child: homeadd_submitButton()
-          ),
-        ],
+              child: homeAdd_Field(),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: homeadd_SubmitButton(context),
+            ),
+          ],
+        ),
+        image: Center(
+          child: Icon(Icons.home, size: 175, color: Colors.lightBlueAccent),),
       ),
-      image: Center(child: Icon(Icons.home,size: 175,color: Colors.lightBlueAccent),),
-    )
-  ];
-
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-    home: IntroductionScreen(
-      pages: listPagesViewModel,
-      showNextButton: false,
-      showBackButton: false,
-      showSkipButton: false,
-      showDoneButton: false,
-      // skip: const Text("Skip"),
-      // done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+      home: IntroductionScreen(
+        pages: listPagesViewModel,
+        showNextButton: false,
+        showBackButton: false,
+        showSkipButton: false,
+        showDoneButton: false,
+        // skip: const Text("Skip"),
+        // done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
 
-    ),
-    );//Material App
-  }
-}
-
-Widget homeadd_textbox(){
-  return Container(
-      decoration: BoxDecoration(
-        color: Color(0x11111111),
-        borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Padding(
-        padding: EdgeInsets.only(left: 15, right: 15, top: 5),
-        child: TextFormField(
-            maxLines: null,
-            keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: "Enter your home's address:",
-            )
-        ),
-      )
-  );
-}
+    ); //Material App
+  }
 
-Widget homeadd_submitButton() {
-  return ElevatedButton(
+  Widget homeAdd_Field() =>
+      TextFormField(
+        keyboardType: TextInputType.multiline,
+        maxLines: null,
+        controller: addresstxt_Controller,
+        decoration: const InputDecoration(
+            labelText: "Home Address",
+            hintText: "Enter your home address here",
+            border: OutlineInputBorder()),
+      );
+
+  Widget homeadd_SubmitButton(BuildContext context) {
+    return ElevatedButton(
       style: ButtonStyle(
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0)
+                borderRadius: BorderRadius.circular(18.0),
               )
           )
       ),
       child: Text("Submit"),
-      onPressed: (){
-        /*Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ???()),*/
+      onPressed: () {
+        setState(() {
+          /* userManager.updateHomeAdd(addresstxt_Controller.text); //Update User's home address
+          );*/
+        });
+        /*context.go('${GoRouter
+            .of(context)
+            .location}/???'); //After setState user info, go to main page*/
       },
     );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    addresstxt_Controller.dispose();
+    super.dispose();
+  }
 }
