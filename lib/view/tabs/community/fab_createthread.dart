62c188f3c9dc5359fe1003c89abbe51dc7c18ad2
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2022-03-28 17:27:25 +0800

    Change fab for creating thread and meetup to be that of stateless.

--- lib/view/tabs/community/fab_createthread.dart
+++ lib/view/tabs/community/fab_createthread.dart
@@ -1,19 +1,21 @@
+import 'package:aura/models/discussion_topic.dart';
 import 'package:flutter/material.dart';
-import 'package:aura/view/community/create_meetup_view.dart';
 import 'package:go_router/go_router.dart';
 
-class FAB_CreateMeetupView extends StatelessWidget {
-  const FAB_CreateMeetupView({Key? key}) : super(key: key);
+class FAB_CreateThread extends StatelessWidget {
+  final DiscussionTopic topic;
+
+  const FAB_CreateThread({Key? key, required this.topic}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(bottom: 50, right: 20),
       child: FloatingActionButton(
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         backgroundColor: Colors.lightBlueAccent,
         onPressed: () {
-          context.push("${GoRouter.of(context).location}/createMeetup");
+          context.push("${GoRouter.of(context).location}/createThread");
         },
       ),
     );
