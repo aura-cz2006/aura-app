c647d596ac002770db06fbcd015068ad0f53fe47
nicolelst <nicolelim2608@gmail.com>
2022-04-03 01:46:46 +0800

    added handling for different news type to api

--- lib/apis/news_api.dart
+++ lib/apis/news_api.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:aura/config/config.dart';
 import 'package:aura/models/news.dart';
-import 'package:http/http.dart' as http; // for making HTTP calls
+import 'package:http/http.dart' as http;
+import 'package:http_status_code/http_status_code.dart';
 
 class NewsApi {
   static Future<List<NewsItem>> fetchNews() async {
@@ -10,21 +11,72 @@ class NewsApi {
 
     http.Response response = await http.get(url);
 
-    if (response.statusCode == 200) {
-      // the response body
+    if (response.statusCode == StatusCode.OK) {
       String responseBody = response.body;
 
       // use dart:convert to decode JSON
-      dynamic decodedJson = json.decode(responseBody);
+      //dynamic decodedJson = json.decode(responseBody); // todo put this back
 
-      List<NewsItem> resList = (decodedJson['news'] as List).map((item) {
-        return UpgradingNewsItem.getFromJson(item);
-      }).toList();
+      Map<String, dynamic> decodedJson = { // todo take this test data out
+        'news': [
+          {
+            'id': '1',
+            'type': 'dengue',
+            'datetime': '2020-01-21 07:21:06',
+            'location': [0.0, 1.0],
+            'numCases': 5
+          },
+          {
+            'id': '2',
+            'type': 'event',
+            'datetime': '2022-04-07 12:30',
+            'location': [0.0, 1.0],
+            'eventTitle': 'VERY EXCITING THINGS ARE HAPPENING',
+            'fee': 9.99,
+            'websiteURL': 'http://api.aura-app.xyz/docs'
+          },
+          {
+            'id': '3',
+            'type': 'market',
+            'datetime': '2022-01-21 07:21:06',
+            'location': [0.0, 1.0],
+            'market': 'big market',
+            'reopeningDate': '2022-03-25 07:21:06'
+          },
+          {
+            'id': '4',
+            'type': 'upgrading',
+            'datetime': '2023-01-21 08:21:06',
+            'location': [0.0, 1.0],
+            'description': 'big fixes',
+            'end': '2023-06-21 07:21:06'
+          }
+        ]
+      };
+
+      List<NewsItem> resList = (decodedJson['news'] as List)
+          .map((item) {
+            switch (item['type']) {
+              case 'dengue': // todo replace w enum names
+                return DengueNewsItem.getFromJson(item);
+              case 'event': // todo replace w enum names
+                return EventNewsItem.getFromJson(item);
+              case 'market': // todo replace w enum names
+                return MarketNewsItem.getFromJson(item);
+              case 'upgrading': // todo replace w enum names
+                return UpgradingNewsItem.getFromJson(item);
+              default:
+                return null;
+            }
+          })
+          .where((newsItem) => newsItem != null)
+          .cast<NewsItem>()
+          .toList();
 
       return resList;
     } else {
-      print("ERROR in getting post: status code: " +
-          response.statusCode.toString());
+      print(
+          "ERROR fetching news: ${response.statusCode} ${getStatusMessage(response.statusCode)}");
       return [];
     }
   }
--- lib/models/news.dart
+++ lib/models/news.dart
@@ -4,11 +4,12 @@ import 'package:intl/intl.dart';
 import 'package:latlong2/latlong.dart';
 
 abstract class NewsItem {
+  String id;
   DateTime dateTime;
-  LatLng location;
+  LatLng location; //todo replace latlng
 
   // constructor
-  NewsItem(this.dateTime, this.location);
+  NewsItem(this.id, this.dateTime, this.location);
 
   static IconData getIcon() {
     return Icons.newspaper;
@@ -20,8 +21,8 @@ abstract class NewsItem {
 class DengueNewsItem extends NewsItem {
   int numCases;
 
-  DengueNewsItem(DateTime dateTime, LatLng location, this.numCases)
-      : super(dateTime, location);
+  DengueNewsItem(String id, DateTime dateTime, LatLng location, this.numCases)
+      : super(id, dateTime, location);
 
   static IconData getIcon() {
     return Icons.bug_report;
@@ -31,6 +32,13 @@ class DengueNewsItem extends NewsItem {
   String getText() {
     return "New dengue cluster detected at $location ($numCases cases).";
   }
+
+  factory DengueNewsItem.getFromJson(Map<String, dynamic> json) {
+    // TODO test
+    DateTime dt = DateTime.parse(json['datetime']);
+    LatLng location = LatLng(json['location'][0], json['location'][1]);
+    return DengueNewsItem(json['id'], dt, location, json['numCases']);
+  }
 }
 
 class EventNewsItem extends NewsItem {
@@ -39,9 +47,9 @@ class EventNewsItem extends NewsItem {
   double fee = 0;
   String websiteURL;
 
-  EventNewsItem(DateTime dateTime, LatLng location, this.eventTitle, this.fee,
-      this.websiteURL)
-      : super(dateTime, location);
+  EventNewsItem(String id, DateTime dateTime, LatLng location, this.eventTitle,
+      this.fee, this.websiteURL)
+      : super(id, dateTime, location);
 
   static IconData getIcon() {
     return Icons.event;
@@ -51,16 +59,25 @@ class EventNewsItem extends NewsItem {
   String getText() {
     return "New event \"$eventTitle\" hosted at $location (fee: $fee).";
   }
+
+  factory EventNewsItem.getFromJson(Map<String, dynamic> json) {
+    // TODO test
+    DateTime dt = DateTime.parse(json['datetime']);
+    LatLng location = LatLng(json['location'][0], json['location'][1]);
+    return EventNewsItem(json['id'], dt, location, json['eventTitle'],
+        json['fee'], json['websiteURL']);
+  }
 }
 
 class MarketNewsItem extends NewsItem {
   String marketName;
   DateTime reopeningDate;
-  List<LatLng> alternativeMarkets = [];
 
-  MarketNewsItem(
-      DateTime dateTime, LatLng location, this.marketName, this.reopeningDate)
-      : super(dateTime, location);
+  // List<LatLng> alternativeMarkets = []; // todo if have time
+
+  MarketNewsItem(String id, DateTime dateTime, LatLng location, this.marketName,
+      this.reopeningDate)
+      : super(id, dateTime, location);
 
   static IconData getIcon() {
     return Icons.shopping_basket;
@@ -70,15 +87,24 @@ class MarketNewsItem extends NewsItem {
   String getText() {
     return "$marketName will be closed until ${DateFormat('yyyy-MM-dd kk:mm').format(reopeningDate)}.";
   }
+
+  factory MarketNewsItem.getFromJson(Map<String, dynamic> json) {
+    // TODO test
+    DateTime dt = DateTime.parse(json['datetime']);
+    LatLng location = LatLng(json['location'][0], json['location'][1]);
+    DateTime reopening_dt = DateTime.parse(json['reopeningDate']);
+    return MarketNewsItem(
+        json['id'], dt, location, json['market'], reopening_dt);
+  }
 }
 
 class UpgradingNewsItem extends NewsItem {
   String desc;
   DateTime expectedEnd;
 
-  UpgradingNewsItem(
-      DateTime dateTime, LatLng location, this.desc, this.expectedEnd)
-      : super(dateTime, location);
+  UpgradingNewsItem(String id, DateTime dateTime, LatLng location, this.desc,
+      this.expectedEnd)
+      : super(id, dateTime, location);
 
   static IconData getIcon() {
     return Icons.construction;
@@ -90,9 +116,11 @@ class UpgradingNewsItem extends NewsItem {
   }
 
   factory UpgradingNewsItem.getFromJson(Map<String, dynamic> json) {
-    DateTime dt = (int.parse(json['id']) % 2 == 0) ? DateTime(2022, 1, 1) : DateTime(2023, 1, 1);
-    return UpgradingNewsItem( // TODO replace: parse api response properly
-        // json['dateTime'], json['location'], json['title'], json['expectedEnd']);
-        dt, LatLng(0, 0), json['title'], DateTime(2022, 3, 31));
+    //todo test
+    DateTime dt = DateTime.parse(json['datetime']);
+    LatLng location = LatLng(json['location'][0], json['location'][1]);
+    DateTime end_dt = DateTime.parse(json['end']);
+    return UpgradingNewsItem(
+        json['id'], dt, location, json['description'], end_dt);
   }
 }
