c898041a97564b510b6cd4b7b93a767590170adc
nicolelst <nicolelim2608@gmail.com>
2022-04-03 01:46:46 +0800

    added handling for different news type to api

--- lib/controllers/news_controller.dart
+++ lib/controllers/news_controller.dart
@@ -7,24 +7,16 @@ import 'package:provider/provider.dart';
 class NewsController {
   static void fetchNews(BuildContext context) async {
 
-    // todo: call api - convert to dart there
-    // todo: receive api data from api handler
-
+    // call api (convert to dart there) and receive api data
     List<NewsItem> fetchedNewsItems = await NewsApi.fetchNews();
 
-    // call provider here
-
+    // call provider
     Provider.of<News_Manager>(context, listen: false).setNews(
         fetchedNewsItems
-      // [
-        //   UpgradingNewsItem(DateTime(2022, 1, 1), LatLng(0, 0),
-        //       "Repainting at Block 5 Dover Road", DateTime(2022, 3, 31)),
-        //   MarketNewsItem(DateTime(2022, 10, 11), LatLng(0, 0),
-        //       "Blk 17 Upper Boon Keng Market and Food Centre", DateTime(2022, 11, 1)),
-        //   EventNewsItem(DateTime(2022, 3, 5), LatLng(0, 0),
-        //       "intro to chromatic harmonica", 10, "https://www.onepa.gov.sg/events"),
-        // ]
     );
-
   }
+
+
+
+
 }
