206fa2c277e7e104f1286e1cfe22933ff32ef0ac
nicolelst <nicolelim2608@gmail.com>
2022-04-03 03:07:29 +0800

    modified settings design to match theme better

--- lib/view/settings/settings_screen.dart
+++ lib/view/settings/settings_screen.dart
@@ -1,4 +1,3 @@
-import 'package:aura/widgets/app_bar_back_button.dart';
 import 'package:aura/widgets/aura_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -13,60 +12,75 @@ class SettingsScreen extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AuraAppBar(
-                title: const Text('Settings'),
+              title: const Text('Settings'),
             ),
             body: Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 20),
-                    child: ChangeHomeButton(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: _changeHomeAddressButton(context),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: _logoutButton(),
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: _logoutButton(context),
                   ),
                 ],
               ),
             )));
   }
-}
 
-Widget _logoutButton(){
-  return ElevatedButton(
-      onPressed: (){},
-      child: Text("Log Out"),
-      style: ElevatedButton.styleFrom(
-        onPrimary: Colors.white, //text
-        primary: Colors.blue,
-        fixedSize: Size(240,50),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-      )
-  );
-}
-
-
-
-class ChangeHomeButton extends StatelessWidget {
-  const ChangeHomeButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _changeHomeAddressButton(BuildContext context) {
     return ElevatedButton(
-      child: Text("Change Home Address"),
-      onPressed: (){
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
+        Icon(
+          Icons.home,
+          size: 30,
+        ),
+        SizedBox(width: 16),
+        Text(
+          "Change Home Address",
+          style: TextStyle(fontSize: 18, color: Colors.black),
+        ),
+      ]),
+      onPressed: () {
         GoRouter.of(context).push("/settings/change_home_address");
         // context.go('${GoRouter.of(context).location}/change_home_address');
       },
       style: ElevatedButton.styleFrom(
-        onPrimary: Colors.white, //text
-        primary: Colors.blue,
-        fixedSize: Size(240,50),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-      )
+        onPrimary: Colors.black,
+        primary: Colors.white,
+        side: const BorderSide(color: Colors.black, width: 2),
+        fixedSize: const Size(300, 70),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      ),
+    );
+  }
+
+  Widget _logoutButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
+        Icon(
+          Icons.logout,
+          size: 30,
+        ),
+        SizedBox(width: 16),
+        Text(
+          "Log Out",
+          style: TextStyle(fontSize: 18, color: Colors.white),
+        ),
+      ]),
+      style: ElevatedButton.styleFrom(
+        onPrimary: Colors.white,
+        primary: Colors.redAccent,
+        fixedSize: const Size(300, 70),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      ),
     );
   }
 }
-
