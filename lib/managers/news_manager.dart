ba6176690cceecd1725198a2eec95cc6dc5909b5
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2022-04-10 21:03:32 +0800

    Changes by ryank

--- lib/managers/news_manager.dart
+++ lib/managers/news_manager.dart
@@ -17,29 +17,34 @@ class News_Manager extends Manager {
   }
 
   List<NewsItem> getNowNewsItems(LatLng curr_user_address) {
-    List<NewsItem> updatedNewsListbyDistance = [];
-    Distance distance = Distance();
-    for (NewsItem news in newsList) {
-      double meter = distance(curr_user_address, news.location);
-      if (meter < 4000) {
-        updatedNewsListbyDistance.add(news);
-      }
-    }
-    return updatedNewsListbyDistance
+    // List<NewsItem> updatedNewsListbyDistance = [];
+    // Distance distance = Distance();
+    // for (NewsItem news in newsList) {
+    //   double meter = distance(curr_user_address, news.location);
+    //   if (meter < 4000) {
+    //     updatedNewsListbyDistance.add(news);
+    //   }
+    // }
+    // return updatedNewsListbyDistance
+    return newsList
         .where((n) =>
             n.dateTime.isBefore(DateTime.now()) || n.dateTime == DateTime.now())
         .toList();
   }
 
   List<NewsItem> getUpcomingNewsItems(LatLng curr_user_address) {
-    List<NewsItem> updatedNewsListbyDistance = [];
-    Distance distance = Distance();
-    for (NewsItem news in newsList) {
-      double meter = distance(curr_user_address, news.location);
-      if (meter < 4000) {
-        updatedNewsListbyDistance.add(news);
-      }
-    }
-    return updatedNewsListbyDistance.where((n) => n.dateTime.isAfter(DateTime.now())).toList();
+    // List<NewsItem> updatedNewsListbyDistance = [];
+    // Distance distance = Distance();
+    // for (NewsItem news in newsList) {
+    //   double meter = distance(curr_user_address, news.location);
+    //   if (meter < 4000) {
+    //     updatedNewsListbyDistance.add(news);
+    //   }
+    // }
+    // return updatedNewsListbyDistance.where((n) => n.dateTime.isAfter(DateTime.now())).toList();
+    return newsList
+        .where((n) =>
+    n.dateTime.isBefore(DateTime.now()) || n.dateTime == DateTime.now())
+        .toList();
   }
 }
